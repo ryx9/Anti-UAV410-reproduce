5c08833971847828f6928bd54d54b1cd2809816d
Rizwan-090505 <rizzwanabdullah@gmail.com>
2026-09-20 16:48:47 +0500

    fix: resotre visdom dep to avoid runtime errors

--- implementation_in_pytracking/pytracking-AntiUAV410/install.sh
+++ implementation_in_pytracking/pytracking-AntiUAV410/install.sh
@@ -136,7 +136,8 @@ python -m pip install \
     cython \
     pycocotools \
     lvis \
-    ninja
+    ninja \
+    visdom
 # ============================================================
 # 7. Install tracker-specific dependencies
 # ============================================================
